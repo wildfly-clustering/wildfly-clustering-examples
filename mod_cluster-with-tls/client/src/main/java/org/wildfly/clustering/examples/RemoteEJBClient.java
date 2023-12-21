7b38b9d51e1d2cc05f5f37d9da4327d1aad64c5d
Richard Achmatowicz <rachmato@redhat.com>
2023-12-21 17:46:46 -0500

    Use clusterbench for deployed application.

--- mod_cluster-with-tls/client/src/main/java/org/wildfly/clustering/examples/RemoteEJBClient.java
+++ mod_cluster-with-tls/client/src/main/java/org/wildfly/clustering/examples/RemoteEJBClient.java
@@ -18,89 +18,34 @@ package org.wildfly.clustering.examples;
 
 import org.jboss.logging.Logger;
 
+import org.jboss.test.clusterbench.ejb.stateless.RemoteStatelessSB;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import java.util.Hashtable;
 
 /**
- * A sample program which acts a remote client for a EJB deployed on JBoss EAP server. This program shows how to lookup stateful and
- * stateless beans via JNDI and then invoke on them
- *
- * @author Jaikiran Pai
+ * A client application which invokes on a stateless session bean RemoteStatelessSB found in the clusterbench
+ * deployment clusterbench-ee10-ejb.ear
  */
 public class RemoteEJBClient {
+    private static final Logger log = Logger.getLogger(RemoteEJBClient.class);
+    private static final String application = "clusterbench-ee10";
+    private static final String module = "clusterbench-ee10-ejb";
+    private static final String bean = "RemoteStatelessSBImpl";
+    private static final String intf = RemoteStatelessSB.class.getName();
 
-    private static final Logger LOGGER = Logger.getLogger(RemoteEJBClient.class);
+    private static final String jndiName = "ejb:" + application + "/" + module + "/" + bean + "!" + intf;
 
     public static void main(String[] args) throws Exception {
-        // Invoke a stateless bean
-        invokeStatelessBean();
-    }
 
-    /**
-     * Looks up a stateless bean and invokes on it
-     *
-     * @throws NamingException
-     */
-    private static void invokeStatelessBean() throws NamingException {
-        // Let's lookup the remote stateless calculator
-        final RemoteCalculator statelessRemoteCalculator = lookupRemoteStatelessCalculator();
-        System.out.println("Obtained a remote stateless calculator for invocation");
-        // invoke on the remote calculator
-        int a = 204;
-        int b = 340;
-        System.out.println("Adding " + a + " and " + b + " via the remote stateless calculator deployed on the server");
-        int sum = statelessRemoteCalculator.add(a, b);
-        System.out.println("Remote calculator returned sum = " + sum);
-        if (sum != a + b) {
-            throw new RuntimeException("Remote stateless calculator returned an incorrect sum " + sum + " ,expected sum was " + (a + b));
-        }
-        // try one more invocation, this time for subtraction
-        int num1 = 3434;
-        int num2 = 2332;
-        System.out.println("Subtracting " + num2 + " from " + num1 + " via the remote stateless calculator deployed on the server");
-        int difference = statelessRemoteCalculator.subtract(num1, num2);
-        System.out.println("Remote calculator returned difference = " + difference);
-        if (difference != num1 - num2) {
-            throw new RuntimeException("Remote stateless calculator returned an incorrect difference " + difference + " ,expected difference was " + (num1 - num2));
-        }
-    }
-
-
-    /**
-     * Looks up and returns the proxy to remote stateless calculator bean
-     *
-     * @return
-     * @throws NamingException
-     */
-    private static RemoteCalculator lookupRemoteStatelessCalculator() throws NamingException {
+        // lookup the bean
         final Hashtable<String, String> jndiProperties = new Hashtable<>();
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
         jndiProperties.put(Context.PROVIDER_URL,"https://localhost:8443/wildfly-services");
-        System.out.println("lookupRemoteStateless:using JNDI Properties map: " + jndiProperties);
-
         final Context context = new InitialContext(jndiProperties);
+        final RemoteStatelessSB remoteStatelessSBProxy = (RemoteStatelessSB) context.lookup(jndiName);
 
-        // The JNDI lookup name for a stateless session bean has the syntax of:
-        // ejb:<appName>/<moduleName>/<distinctName>/<beanName>!<viewClassName>
-        //
-        // <appName> The application name is the name of the EAR that the EJB is deployed in
-        // (without the .ear). If the EJB JAR is not deployed in an EAR then this is
-        // blank. The app name can also be specified in the EAR's application.xml
-        //
-        // <moduleName> By the default the module name is the name of the EJB JAR file (without the
-        // .jar suffix). The module name might be overridden in the ejb-jar.xml
-        //
-        // <distinctName> : EAP allows each deployment to have an (optional) distinct name.
-        // This example does not use this so leave it blank.
-        //
-        // <beanName> : The name of the session been to be invoked.
-        //
-        // <viewClassName>: The fully qualified classname of the remote interface. Must include
-        // the whole package name.
-
-        // let's do the lookup
-        return (RemoteCalculator) context.lookup("ejb:/server/CalculatorBean!" + RemoteCalculator.class.getName());
-    }
-}
+        // Invoke on the stateless bean
+        log.infof("Invoking method getNodeName(): result = %s", remoteStatelessSBProxy.getNodeName());
+     }
+ }
